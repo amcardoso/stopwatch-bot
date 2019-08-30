243d0679105486886011ce1549bc142b404f2203
Anderson <anderson1@br.ibm.com>
2019-08-30 09:25:06 -0300

    Criando rota para o webhook do telegram

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,9 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const receiveMessage = functions.https.onRequest((request, response) => {
+  console.log(request.headers);
+  console.log(request.url);
+  console.log(request.body);
+  response.send("OK");
 });
